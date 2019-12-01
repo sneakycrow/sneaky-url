b3e483908eab40620e581e4340902d4cd6fbc1c2
Zachary E Sohovich <zacharyesohovich@gmail.com>
2019-12-01 14:20:45 -0800

    removed post conditional wrapper

--- api/create-link.ts
+++ api/create-link.ts
@@ -15,35 +15,34 @@ mutation newLinkMutation($id: String, $urlTarget: String) {
 
 const handler = (req: NowRequest, res: NowResponse) => {
   if (req.headers['x-sneaky-key'] && req.headers['x-sneaky-key'] === process.env.sneaky_key) {
-    if (req.method === 'POST') {
-      if (req.body) {
-        const { urlTarget } = req.body;
-        const id = shortid.generate();
-        if (urlTarget && id) {
-          const variables = {
-            id,
-            urlTarget
-          };
+    if (req.body) {
+      const { urlTarget } = req.body;
+      const id = shortid.generate();
+      if (urlTarget && id) {
+        const variables = {
+          id,
+          urlTarget
+        };
 
-          const graphQLClient = new GraphQLClient(process.env.graphql_endpoint, {
-            headers: {
-              ['x-hasura-admin-secret']: process.env.graphql_key
-            }
-          });
+        const graphQLClient = new GraphQLClient(process.env.graphql_endpoint, {
+          headers: {
+            ['x-hasura-admin-secret']: process.env.graphql_key
+          }
+        });
 
-          return graphQLClient.request(newLinkMutation, variables).then(data => {
-            const { affected_rows, returning } = data.insert_sneaky_url;
-            const baseURL = 'https://sneakycrow.dev';
-            if (affected_rows > 0) {
-              return res.status(200).json({ status: 'success', newUrl: `${baseURL}/${returning[0].id}` });
-            }
-            return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
-          });
-        }
+        return graphQLClient.request(newLinkMutation, variables).then(data => {
+          const { affected_rows, returning } = data.insert_sneaky_url;
+          const baseURL = 'https://sneakycrow.dev';
+          if (affected_rows > 0) {
+            return res
+              .status(200)
+              .json({ status: 'success', newUrl: `${baseURL}/${returning[0].id}` });
+          }
+          return res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+        });
       }
-      return res.status(400).json({ error: 'This endpoint requires a url target' });
     }
-    return res.status(400).json({ error: 'This endpoint only allows POST requests' });
+    return res.status(400).json({ error: 'This endpoint requires a url target' });
   }
   res.status(401).json({ error: 'Unauthorized Access' });
 };
